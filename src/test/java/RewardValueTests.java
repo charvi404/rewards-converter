d3fbeec1e8024c133231c63792851c30d806d9b5
charvi404 <charvi.srivastava2020@gmail.com>
2024-06-23 21:08:26 +0530

    Revert "Add unit tests for RewardValue class and fix existing tests"

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,35 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
-
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new main.java.RewardValue(cashValue);
+        var rewardValue = new RewardValue(cashValue);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new main.java.RewardValue(milesValue, true);
+        var rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new main.java.RewardValue(cashValue);
-        double expectedMiles = cashValue / MILES_TO_CASH_CONVERSION_RATE;
-        assertEquals(expectedMiles, rewardValue.getMilesValue());
+        assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        double milesValue = 10000;
-        var rewardValue = new main.java.RewardValue(milesValue, true);
-        double expectedCash = milesValue * MILES_TO_CASH_CONVERSION_RATE;
-        assertEquals(expectedCash, rewardValue.getCashValue());
+        assert false;
     }
 }
